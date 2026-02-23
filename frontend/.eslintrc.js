cf53d588e69d43227ab0fbdb516982c09126a63c
SAMITH-07 <your-email@example.com>
2026-02-24 02:43:20 +0530

    Disable all ESLint rules in .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   extends: ['react-app', 'react-app/jest'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'jsx-a11y/anchor-is-valid': 'warn',
-    'react-hooks/exhaustive-deps': 'warn'
+    '@typescript-eslint/no-unused-vars': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-unused-vars': 'off'
   }
 };
